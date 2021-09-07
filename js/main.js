b1f21c5de047747af8509d06e47d3b2b91c77dbc
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-09-06 20:07:49 -0500

    Update 2nd Requirement

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
+document.getElementById("create-button").onclick = function (argument) {
+	
 
-
-
-
+	let pet = {
+		dateInput: document.getElementById("date-input").value,
+		ownerInput: document.getElementById("owner-input").value,
+		petNameInput: document.getElementById("petname-input").value,
+		petSpeciesInput: document.getElementById("petspecies-select").value,
+		petSizeInput: document.getElementById("petsize-select").value,
+	}
+	let ownerInput = document.getElementById("owner-input").value;
+	document.cookie = "ownername="+ownerInput;
+}
