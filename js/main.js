33a8cb82ba97e101635fd35b239b34b469e78732
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-09-07 21:02:11 -0500

    Created cookies

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-document.getElementById("create-button").onclick = function (argument) {
+document.getElementById("create-button").onclick = function () {
 	
+	let dateInput = document.getElementById("date-input").value;
+	document.cookie = "dateInput=" + dateInput;
 
-	let pet = {
-		dateInput: document.getElementById("date-input").value,
-		ownerInput: document.getElementById("owner-input").value,
-		petNameInput: document.getElementById("petname-input").value,
-		petSpeciesInput: document.getElementById("petspecies-select").value,
-		petSizeInput: document.getElementById("petsize-select").value,
-	}
-	let ownerInput = document.getElementById("owner-input").value;
-	document.cookie = "ownername="+ownerInput;
-}
+	let ownerName = document.getElementById("name-input").value;
+	document.cookie = "ownerName=" + ownerName;
+
+	let petName = document.getElementById("petName-input").value;
+	document.cookie = "petName=" + petName;
+
+	let petSpecies = document.getElementById("petspecie-select").value;
+	document.cookie = "petSpecies=" + petSpecies;
+
+	let petSex= document.getElementById("petsex-select").value;
+	document.cookie = "petSex=" + petSex;
+
+	let petSize = document.getElementById("petsize-select").value;
+	document.cookie = "petSize=" + petSize;
+
+	let petDanger = document.getElementById("petdanger-select").value;
+	document.cookie = "petDanger=" + petDanger;
+
+	let petSterilized = document.getElementById("petsterilized-select").value;
+	document.cookie = "petSterilized=" + petSterilized;
+
+	let petNeigborhood = document.getElementById("petneigborhood-select").value;
+	document.cookie = "petNeigborhood=" + petNeigborhood;
+};
+	
+	
